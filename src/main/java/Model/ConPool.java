4c6d08b57145d69111b3c9dd801f32f98a4c9c9f
Onedrift <fantasia.6549658256@gmail.com>
2025-05-28 10:03:39 +0200

    Update ConPool.java

--- src/main/java/Model/ConPool.java
+++ src/main/java/Model/ConPool.java
@@ -17,7 +17,7 @@ public class ConPool {
             p.setUrl("jdbc:mysql://localhost:3306/progettotsw?serverTimezone=" + TimeZone.getDefault().getID());
             p.setDriverClassName("com.mysql.cj.jdbc.Driver");
             p.setUsername("root");
-            p.setPassword("studentiTSW");
+            p.setPassword("Filmgratis12?");
             p.setMaxActive(100);
             p.setInitialSize(10);
             p.setMinIdle(10);
